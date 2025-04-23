4d28705c763d5332fa839568d4703983599fd486
Taras Shevchuk <taras.shevchuk0024@gmail.com>
2025-04-23 18:53:28 +0200

    Update src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Consumer.java
    Co-authored-by: Mykola Digtiar <mykola.digtiar@gmail.com>

--- src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Consumer.java
+++ src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Consumer.java
@@ -1,8 +1,10 @@
 package gmail.taras.shevchuk0024.lesson40_Functional_Interface;
 
-public class Consumer {
-    public static java.util.function.Consumer<Integer> giveRandomNumber = maxAmount -> {
-        int result = (int) (Math.random() * (maxAmount + 1));
+import java.util.function.Consumer;
+
+public class ConsumerUtils {
+    public static Consumer<Integer> giveRandomNumber = maxNumber -> {
+        int result = (int) (Math.random() * (maxNumber + 1));
         System.out.println("Випадкове число: " + result);
     };
 }
