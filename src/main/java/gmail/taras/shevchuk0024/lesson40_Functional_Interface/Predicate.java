1204792417bbc080c0671dc8517834a2e87d74e7
Taras Shevchuk <taras.shevchuk0024@gmail.com>
2025-04-23 18:53:41 +0200

    Update src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Predicate.java
    Co-authored-by: Mykola Digtiar <mykola.digtiar@gmail.com>

--- src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Predicate.java
+++ src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Predicate.java
@@ -1,11 +1,19 @@
 package gmail.taras.shevchuk0024.lesson40_Functional_Interface;
 
-public class Predicate {
-    public static java.util.function.Predicate<Integer> isSimple = number -> {
-        if (number <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
+import java.util.function.Predicate;
+
+public class PredicateUtils {
+    public static Predicate<Integer> isPrime = number -> {
+        if (number <= 1) {
+            return false;
         }
+
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+
         return true;
     };
 }
