bb7dd14bbd07e82ee60111f031ac20490a867250
Taras Shevchuk <taras.shevchuk0024@gmail.com>
2025-04-23 18:54:00 +0200

    Update src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/UnaryOperator.java
    Co-authored-by: Mykola Digtiar <mykola.digtiar@gmail.com>

--- src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/UnaryOperator.java
+++ src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/UnaryOperator.java
@@ -1,16 +1,26 @@
 package gmail.taras.shevchuk0024.lesson40_Functional_Interface;
 
-public class UnaryOperator {
-    public static java.util.function.UnaryOperator<Integer> fibonacci = n -> {
-        if (n == 0) return 0;
-        if (n == 1) return 1;
+import java.util.function.UnaryOperator;
 
-        int a = 0, b = 1;
+public class UnaryOperatorUtils {
+    public static UnaryOperator<Integer> fibonacci = n -> {
+        if (n == 0) {
+            return 0;
+        }
+        
+        if (n == 1) {
+            return 1;
+        }
+
+        int a = 0;
+        int b = 1;
+
         for (int i = 2; i <= n; i++) {
             int temp = a + b;
             a = b;
             b = temp;
         }
+
         return b;
     };
 }
