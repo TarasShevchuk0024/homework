324f46bf446b65646bee3e7574b9b1b41cb1df29
Taras Shevchuk <taras.shevchuk0024@gmail.com>
2025-04-23 18:53:18 +0200

    Update src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Function.java
    Co-authored-by: Mykola Digtiar <mykola.digtiar@gmail.com>

--- src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Function.java
+++ src/main/java/gmail/taras/shevchuk0024/lesson40_Functional_Interface/Function.java
@@ -1,5 +1,7 @@
 package gmail.taras.shevchuk0024.lesson40_Functional_Interface;
 
-public class Function {
-    public static final java.util.function.Function<Double, Long> roundToLong = d -> Math.round(d);
+import java.util.function.Function;
+
+public class FunctionUtils {
+    public static Function<Double, Long> roundToLong = d -> Math.round(d);
 }
